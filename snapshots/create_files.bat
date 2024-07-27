f7fc2e4413fc38394e4e0fc1b07488e98445d7a7
rassel25 <36706178+rassel25@users.noreply.github.com>
2024-07-27 14:05:26 +0200

    first commit

---
+++ snapshots/create_files.bat
@@ -0,0 +1,12 @@
+@echo off
+copy nul address.sql
+copy nul customer.sql
+copy nul customeraddress.sql
+copy nul product.sql
+copy nul productcategory.sql
+copy nul productdescription.sql
+copy nul productmodel.sql
+copy nul productmodelproductdescription.sql
+copy nul salesorderdetail.sql
+copy nul salesorderheader.sql
+echo Files created.
